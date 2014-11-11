6b64b5d4f61350912237ac0cdd9343d6a1fc58a7
Kenny Wong <kwong.dev@gmail.com>
2014-11-11 16:19:19 -0500

    Working mediaextractor/mediacodec etc

--- src/org/vinuxproject/sonic/SonicTest.java
+++ src/org/vinuxproject/sonic/SonicTest.java
@@ -7,6 +7,7 @@
 
 package org.vinuxproject.sonic;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
@@ -18,21 +19,20 @@ import android.media.MediaCodec.BufferInfo;
 import android.media.MediaExtractor;
 import android.media.MediaFormat;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
-import android.view.Surface;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class SonicTest extends Activity {
-//	public static final int SAMPLING_FREQUENCY = 44100;
-	public static final int SAMPLING_FREQUENCY = 22050;
+	public static int SAMPLING_FREQUENCY = 44100;
+	public static int CHANNEL_COUNT = 2;
+//	public static final int SAMPLING_FREQUENCY = 22050;
 	float speed = 1.0f;
 	private PlayerThread mPlayer = null;
+	
+	byte[] audioArray = null;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -61,6 +61,7 @@ public class SonicTest extends Activity {
 		
 		if (mPlayer == null) {
 			mPlayer = new PlayerThread();
+			Log.v("SonicTest", "starting decoding");
 			mPlayer.start();
 		}
 	}
@@ -75,11 +76,11 @@ public class SonicTest extends Activity {
             	final EditText rateEdit = (EditText) findViewById(R.id.rate);
             	float pitch = Float.parseFloat(pitchEdit.getText().toString());
             	float rate = Float.parseFloat(rateEdit.getText().toString());
-                AndroidAudioDevice device = new AndroidAudioDevice(SAMPLING_FREQUENCY, 1);
-                Sonic sonic = new Sonic(SAMPLING_FREQUENCY, 1);
+                AndroidAudioDevice device = new AndroidAudioDevice(SAMPLING_FREQUENCY, CHANNEL_COUNT);
+                Sonic sonic = new Sonic(SAMPLING_FREQUENCY, CHANNEL_COUNT);
                 byte samples[] = new byte[4096];
                 byte modifiedSamples[] = new byte[2048];
-                InputStream soundFile = getResources().openRawResource(R.raw.talking);
+                InputStream soundFile = new ByteArrayInputStream(audioArray);
 
 				if(soundFile != null) {
 				    
@@ -124,6 +125,12 @@ public class SonicTest extends Activity {
 		@Override
 		public void run() {
 			extractor = new MediaExtractor();
+//			try {
+//				extractor.setDataSource(Environment.getExternalStorageDirectory().getPath() + "/Download/whatohyeah.mp3");
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
 			AssetFileDescriptor afd = getApplicationContext().getResources().openRawResourceFd(R.raw.howlingabyss);
 			if (afd == null) return;
 			try {
@@ -136,16 +143,19 @@ public class SonicTest extends Activity {
 
 			for (int i = 0; i < extractor.getTrackCount(); i++) {
 				MediaFormat format = extractor.getTrackFormat(i);
+				SAMPLING_FREQUENCY = format.getInteger(MediaFormat.KEY_SAMPLE_RATE);
+				CHANNEL_COUNT = format.getInteger(MediaFormat.KEY_CHANNEL_COUNT);
 				String mime = format.getString(MediaFormat.KEY_MIME);
 				if (mime.startsWith("audio/")) {
 					extractor.selectTrack(i);
 					decoder = MediaCodec.createDecoderByType(mime);
+					decoder.configure(format, null, null, 0);
 					break;
 				}
 			}
 
 			if (decoder == null) {
-				Log.e("DecodeActivity", "Can't find video info!");
+				Log.e("DecodeActivity", "Can't find audio info!");
 				return;
 			}
 
@@ -155,63 +165,65 @@ public class SonicTest extends Activity {
 			ByteBuffer[] outputBuffers = decoder.getOutputBuffers();
 			BufferInfo info = new BufferInfo();
 			boolean isEOS = false;
-			long startMs = System.currentTimeMillis();
 
-			while (!Thread.interrupted()) {
-				if (!isEOS) {
-					int inIndex = decoder.dequeueInputBuffer(10000);
-					if (inIndex >= 0) {
-						ByteBuffer buffer = inputBuffers[inIndex];
-						int sampleSize = extractor.readSampleData(buffer, 0);
-						if (sampleSize < 0) {
-							// We shouldn't stop the playback at this point, just pass the EOS
-							// flag to decoder, we will get it again from the
-							// dequeueOutputBuffer
-							Log.d("DecodeActivity", "InputBuffer BUFFER_FLAG_END_OF_STREAM");
-							decoder.queueInputBuffer(inIndex, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM);
-							isEOS = true;
-						} else {
-							decoder.queueInputBuffer(inIndex, 0, sampleSize, extractor.getSampleTime(), 0);
-							extractor.advance();
-						}
-					}
-				}
+			while (true) {
+			    if (!isEOS) {
+			        int inIndex = decoder.dequeueInputBuffer(10000);
+			        if (inIndex >= 0) {
+			            ByteBuffer buffer = inputBuffers[inIndex];
+			            int sampleSize = extractor.readSampleData(buffer, 0);
+			            if (sampleSize < 0) {
+			                // We shouldn't stop the playback at this point, just pass the EOS
+			                // flag to decoder, we will get it again from the
+			                // dequeueOutputBuffer
+			                Log.d("DecodeActivity", "InputBuffer BUFFER_FLAG_END_OF_STREAM");
+			                decoder.queueInputBuffer(inIndex, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM);
+			                isEOS = true;
+			            } else {
+			                decoder.queueInputBuffer(inIndex, 0, sampleSize, extractor.getSampleTime(), 0);
+			                extractor.advance();
+			            }
+			        }
+			    }
 
-				int outIndex = decoder.dequeueOutputBuffer(info, 10000);
-				switch (outIndex) {
-				case MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED:
-					Log.d("DecodeActivity", "INFO_OUTPUT_BUFFERS_CHANGED");
-					outputBuffers = decoder.getOutputBuffers();
-					break;
-				case MediaCodec.INFO_OUTPUT_FORMAT_CHANGED:
-					Log.d("DecodeActivity", "New format " + decoder.getOutputFormat());
-					break;
-				case MediaCodec.INFO_TRY_AGAIN_LATER:
-					Log.d("DecodeActivity", "dequeueOutputBuffer timed out!");
-					break;
-				default:
-					ByteBuffer buffer = outputBuffers[outIndex];
-					Log.v("DecodeActivity", "We can't use this buffer but render it due to the API limit, " + buffer);
+			    int outIndex = decoder.dequeueOutputBuffer(info, 10000);
+			    switch (outIndex) {
+			    case MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED:
+			        Log.d("DecodeActivity", "INFO_OUTPUT_BUFFERS_CHANGED");
+			        outputBuffers = decoder.getOutputBuffers();
+			        break;
+			    case MediaCodec.INFO_OUTPUT_FORMAT_CHANGED:
+			        Log.d("DecodeActivity", "New format " + decoder.getOutputFormat());
+			        break;
+			    case MediaCodec.INFO_TRY_AGAIN_LATER:
+			        Log.d("DecodeActivity", "dequeueOutputBuffer timed out!");
+			        break;
+			    default:
+			        ByteBuffer buffer = outputBuffers[outIndex];
+			        // How to obtain PCM samples from this buffer variable??
+			        byte[] b = new byte[info.size-info.offset];                         
+			        int a = buffer.position();
+			        buffer.get(b);
+			        buffer.position(a);
+			        
+			        if (audioArray == null) {
+			        	audioArray = b;
+			        } else {
+			        	byte[] c = new byte[audioArray.length + b.length];
+			        	System.arraycopy(audioArray, 0, c, 0, audioArray.length);
+			        	System.arraycopy(b, 0, c, audioArray.length, b.length);
+			        	audioArray = c;
+			        }
+			        
+			        decoder.releaseOutputBuffer(outIndex, true);
+			        break;
+			    }
 
-					// We use a very simple clock to keep the video FPS, or the video
-					// playback will be too fast
-					while (info.presentationTimeUs / 1000 > System.currentTimeMillis() - startMs) {
-						try {
-							sleep(10);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-							break;
-						}
-					}
-					decoder.releaseOutputBuffer(outIndex, true);
-					break;
-				}
-
-				// All decoded frames have been rendered, we can stop playing now
-				if ((info.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
-					Log.d("DecodeActivity", "OutputBuffer BUFFER_FLAG_END_OF_STREAM");
-					break;
-				}
+			    // All decoded frames have been rendered, we can stop playing now
+			    if ((info.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
+			        Log.d("DecodeActivity", "OutputBuffer BUFFER_FLAG_END_OF_STREAM");
+			        break;
+			    }
 			}
 
 			decoder.stop();
